9060b0e1708ef45263508cb3158a256ebb1f1c34
JoeyWiharta <joeywiharta23@gmail.com>
2026-01-21 21:30:26 +0700

    Check Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-    base: process.env.VITE_ENV === "production" ? "/plantya/" : "/plantya/",  
-    // base: "/plantya/", ==== Ato pake ini ====
-})
+  base: process.env.VITE_ENV === "production" ? "/plantya/" : "/plantya/",
 
-// export default defineConfig({
-//   base: process.env.VITE_APP_APIKEY === "production" ? "/sf/" : "/sf/",
-//   plugins: [react()],
-//   esbuild: {
-//     jsxFactory: 'h',
-//     jsxFragment: 'Fragment',
-//   }
-// })
+  // esbuild: {
+  //     jsxFactory: 'h',
+  //     jsxFragment: 'Fragment',
+  //   }
+
+})
