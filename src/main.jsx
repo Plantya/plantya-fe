36e1ac17c29cbe2205ef4487bc59547f3ee01327
JoeyWiharta <joeywiharta23@gmail.com>
2026-01-22 23:14:26 +0700

    progress dark / light

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/plantya">
       <AuthProvider>
-        <ThemeProvider theme={darkTheme}>
+        <ThemeProvider theme={darkTheme}> 
           <CssBaseline />
           <App />
         </ThemeProvider>
@@ -23,3 +23,31 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode >
 )
+
+// // Prepare for dark mode / light
+// const Root = () => {
+//   const [mode, setMode] = useState("dark")
+//   const theme = useMemo(
+//     () => createTheme(darkTheme(mode)),
+//     [mode]
+//   );
+
+//   return (
+//     <BrowserRouter basename="/plantya">
+//       <AuthProvider>
+//         <ThemeProvider theme={theme}>
+//           <CssBaseline />
+//           <App toggleTheme={() => setMode(p => p === "dark" ? "light" : "dark")} />
+//         </ThemeProvider>
+//       </AuthProvider>
+//     </BrowserRouter>
+//   );
+// };
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <Root />
+//   </React.StrictMode>
+// );
+
+
